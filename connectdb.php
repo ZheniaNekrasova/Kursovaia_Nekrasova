349ea0a277f33aef6eee33e9f4088abf5f6a1428
ZheniaNekrasova <83225558+ZheniaNekrasova@users.noreply.github.com>
2022-01-21 01:27:04 +0300

    Подключила бд

---
+++ connectdb.php
@@ -0,0 +1,3 @@
+<?php
+$connect = mysqli_connect("std-mysql.ist.mospolytech.ru", "std_1585_park", "Hello13day", "std_1585_park");
+?>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <div class="container">
         <nav class="main-menu">
             <a href="index.php"><img class="logo" src="images\logo.png"></a>
-            <a  href="index.php">Парковые территории Москвы</a>
+            <a href="index.php">Парковые территории Москвы</a>
             <a class="right" href="index.php">Главная</a>
             <a class="right" href="">Списки</a>
             <a class="right" href="">Где мне погулять?</a>
@@ -25,15 +25,10 @@
         <section id="about">
             <h1>О нас</h1>
             Хочешь гулять, но не знаешь где? Тогда ты попал по адресу!
-            <figure class="avatar" >
-                <img src="images\1.jpg">
-            </figure>
+            <?=$content?>
             <div class="text-right">
-                <a class="btn" href="">Подробнее</a>
+                <a class="btn" href="page.php">Подробнее</a>
             </div>
-            <p>
-
-            </p>
         </section>
     </div>
 </main>
@@ -43,7 +38,7 @@
         <p>
         <?php
             date_default_timezone_set("Europe/Moscow");
-            echo '<p class="container">&copy; Некрасова Е.М. Сформированно: '.date("d.m.y в H-i.s").'<p>';
+            echo '<p class="container">&copy; Некрасова Е.М.    Сформированно: '.date("d.m.y в H-i.s").'<p>';
             ?>
         </p>
     </div>
